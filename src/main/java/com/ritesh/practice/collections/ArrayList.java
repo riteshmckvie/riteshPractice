91b6f655686812d431f4c33254d7cc9eaf024431
Ritesh Tiwari <ritesh.mckvie@gmail.com>
2019-02-15 12:16:14 +0530

    array list modification

---
+++ src/main/java/com/ritesh/practice/collections/ArrayList.java
@@ -0,0 +1,16 @@
+package com.ritesh.practice.collections;
+
+import java.util.List;
+
+public class ArrayList {
+
+	/**
+	 * 
+	 */
+	public static void main(String[] args) {
+		List<String> list = new java.util.ArrayList<>();
+		list.add("a");
+		System.out.println(list.get(0));
+	}
+	
+}
