3912a659a3d7e887a9182025e1f6f5d10f61954e
Ritesh Tiwari <ritesh.mckvie@gmail.com>
2019-02-16 23:27:04 +0530

    set example

--- src/main/java/com/ritesh/practice/collections/List.java
+++ src/main/java/com/ritesh/practice/collections/List.java
@@ -2,8 +2,12 @@ package com.ritesh.practice.collections;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Vector;
 
 public class List {
+
+	private static boolean add;
 
 	/**
 	 * 
@@ -13,45 +17,58 @@ public class List {
 		list.add("mahesh");
 		list.add("ami");
 		Iterator<String> elemIter = list.iterator();
+		performLinkedList();
+		performVectorList();
+		performStackList();
 
-		while(elemIter.hasNext()) {
+		while (elemIter.hasNext()) {
 			System.out.println(elemIter.next());
 			elemIter.remove();
 
 		}
-		/*for(String elem:list) {
-			System.out.println(elem);
-		}*/
-		
-		
-		
-		
-		// Create a link list which stores integer elements 
-        java.util.List<Integer> l = new LinkedList<Integer>(); 
-  
-        // Now add elements to the Link List 
-        l.add(2); 
-        l.add(3); 
-        l.add(4); 
-  
-        // Make another Link List which stores integer elements 
-        java.util.List<Integer> s=new LinkedList<Integer>(); 
-        s.add(7); 
-        s.add(8); 
-        s.add(9); 
-  
-        // Iterator to iterate over a Link List 
-        for (Iterator<Integer> itr1=l.iterator(); itr1.hasNext(); ) 
-        { 
-            for (Iterator<Integer> itr2=s.iterator(); itr2.hasNext(); ) 
-            { 
-                if (itr1.next() < itr2.next()) 
-                { 
-                    System.out.println(itr1.next()); 
-                } 
-            } 
-        } 
-		
+
+		// Create a link list which stores integer elements
+		java.util.List<Integer> l = new LinkedList<Integer>();
+
+		// Now add elements to the Link List
+		l.add(2);
+		l.add(3);
+		l.add(4);
+
+		// Make another Link List which stores integer elements
+		java.util.List<Integer> s = new LinkedList<Integer>();
+		s.add(7);
+		s.add(8);
+		s.add(9);
+
+		// Iterator to iterate over a Link List
+		for (Iterator<Integer> itr1 = l.iterator(); itr1.hasNext();) {
+			for (Iterator<Integer> itr2 = s.iterator(); itr2.hasNext();) {
+				if (itr1.next() < itr2.next()) {
+					System.out.println(itr1.next());
+				}
+			}
+		}
+
 	}
-	
+
+	private static void performStackList() {
+ Stack<String> stack = new Stack<>();
+ stack.push("12");
+ add = stack.add("13");//uses vector while addition
+ 
+	}
+
+	private static void performVectorList() {
+		Vector<String> vector = new Vector<>();
+		vector.add("rag");
+	}
+
+	private static void performLinkedList() {
+		java.util.List<String> linkedList = new LinkedList<>();
+		linkedList.add("Ritesh");
+		linkedList.forEach(p -> System.out.println(p));
+		linkedList.forEach(System.out::println);
+	}
+
 }
---
+++ src/main/java/com/ritesh/practice/collections/Set.java
@@ -0,0 +1,9 @@
+package com.ritesh.practice.collections;
+
+public class Set {
+
+	public static void main(String[] args) {
+
+	}
+
+}
