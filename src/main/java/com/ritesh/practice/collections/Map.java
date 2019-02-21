3b7e7a16014384a5e60d2332d60d5ebf857f9661
Ritesh Tiwari <ritesh.mckvie@gmail.com>
2019-02-21 13:20:59 +0530

    set and map class added

---
+++ src/main/java/com/ritesh/practice/collections/Map.java
@@ -0,0 +1,10 @@
+package com.ritesh.practice.collections;
+
+public class Map {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- src/main/java/com/ritesh/practice/collections/Set.java
+++ src/main/java/com/ritesh/practice/collections/Set.java
@@ -1,9 +1,13 @@
 package com.ritesh.practice.collections;
 
+import java.util.HashSet;
+
 public class Set {
 
 	public static void main(String[] args) {
 
+		java.util.Set<String> bookSet = new HashSet<>();
+		bookSet.add("ritesh");
 	}
 
 }
