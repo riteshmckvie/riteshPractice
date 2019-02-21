b0211515c0f4fa11475f872bb7df4fdaa211fd40
Ritesh Tiwari <ritesh.mckvie@gmail.com>
2019-02-21 13:17:07 +0530

    stack added

--- src/main/java/com/ritesh/practice/collections/Stack.java
+++ src/main/java/com/ritesh/practice/collections/Stack.java
@@ -3,7 +3,8 @@ package com.ritesh.practice.collections;
 public class Stack {
 
 	public static void main(String[] args) {
-
-	}
+		java.util.Stack<Integer> stack = new java.util.Stack<>();
+		stack.push(1001);
+		}
 
 }
