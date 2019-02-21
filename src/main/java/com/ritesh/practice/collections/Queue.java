bb7827b3db3a68c3c1955a3ef02d85057f1bd5fb
Ritesh Tiwari <ritesh.mckvie@gmail.com>
2019-02-21 11:53:28 +0530

    added stack and queue classes

--- src/main/java/com/ritesh/practice/collections/Queue.java
+++ src/main/java/com/ritesh/practice/collections/Queue.java
@@ -1,9 +1,22 @@
 package com.ritesh.practice.collections;
 
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
 public class Queue {
 
 	public static void main(String[] args) {
-
+		java.util.Queue<Integer> q = new LinkedList<>(); 
+		q.add(1);
+		q.add(2);
+		q.remove();
+		System.out.println(q);
+		
+		java.util.Queue<Integer> priorityQ =new PriorityQueue<>();  
+		priorityQ.add(1);
+		priorityQ.add(2);
+		System.out.println(priorityQ);
+		
 	}
 
 }
---
+++ src/main/java/com/ritesh/practice/collections/Stack.java
@@ -0,0 +1,9 @@
+package com.ritesh.practice.collections;
+
+public class Stack {
+
+	public static void main(String[] args) {
+
+	}
+
+}
