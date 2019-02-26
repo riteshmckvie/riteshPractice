3eb998115ff3dd1d5250cb043064f529a24996f6
Ritesh Tiwari <ritesh.mckvie@gmail.com>
2019-02-26 14:17:10 +0530

    wait notify and notify all example

---
+++ src/main/java/com/ritesh/practice/threading/Notifier.java
@@ -0,0 +1,31 @@
+package com.ritesh.practice.threading;
+
+public class Notifier implements Runnable {
+
+	private Message msg;
+
+	public Notifier(Message msg) {
+		this.msg = msg;
+	}
+
+	@Override
+	public void run() {
+		String name = Thread.currentThread().getName();
+		System.out.println(name + " started");
+		try {
+			Thread.sleep(1000);
+			synchronized (msg) {
+				msg.setMsg(name + " Notifier work done");
+				//msg.notify();
+
+				msg.wait();
+				msg.notifyAll();
+				System.out.println(".....");
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
